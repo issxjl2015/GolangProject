98fa8c6231339072c4561bb74c64001d378253cb
issxjl2015 <issxjl2015@outlook.com>
2019-07-24 20:11:47 +0800

    分离出Hello函数，定义函数时使用关键字string，意味着函数返回一个字符串

--- src/HelloWorld.go
+++ src/HelloWorld.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println(Hello())
 	fmt.Println("Hello World")
+}
+
+// 使用func() 创建出一个新的函数
+// 定义时增加了关键字：string，
+// 意味着这个函数返回一个字符串
+func Hello() string {
+	return "Hello World"
 }
